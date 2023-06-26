f5631debf6280aee41755ab4ff6f81e70cf3f2f6
Kamenriderx <53202711+Kamenriderx@users.noreply.github.com>
2023-06-25 21:28:12 -0600

    Change password

--- client/src/views/Auth/components/ConfirmAccountForm.jsx
+++ client/src/views/Auth/components/ConfirmAccountForm.jsx
@@ -1,20 +1,77 @@
+import { GiLynxHead } from "react-icons/gi";
+import { httpRequests } from "../../../utils/helpers/httpRequests";
+import { useState } from "react";
+
 const ConfirmAccountForm = () => {
-    return ( 
+  const [accountNumber, setAccountNumber] = useState("");
 
-        <form action="" className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex flex-col items-center space-x-4">
-            <div className="text-center space-y-2 sm:text-left">
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setAccountNumber(value);
+  };
 
-                <p className="text-lg text-black font-semibold">
-                    Nueva contraseña
-                </p>
-                <input type="text" className="border border-r-indigo-500"  placeholder="Contraseña"/>
-                <input type="text" className="focus:outline-none border-r-indigo-500" placeholder="Repite tu contraseña"/>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const regularExpression = /^\d{11}$/;
+    if(!regularExpression.test(accountNumber)){
+      alert("El numero de cuenta ingresado no es valido");
+      return;
+    }
 
-            </div>
-                <button  className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Enviar</button>
-
-        </form>
+    const response = await httpRequests()["put"](
+      `http://localhost:3000/registro/resetPassword/sendRestoreEmail/${accountNumber}`,
+      {}
     );
-}
+    if(response.status === 200){
+      alert(response.data.message);
+    }else{
+      alert("Algo salio mal");
+
+    }
+  };
+  return (
+    <form
+      action=""
+      className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex-col items-center space-x-4 animate__zoomIn animate__animated"
+    >
+      <div className="flex flex-row justify-center items-center mb-3">
+        <GiLynxHead style={{ width: "40px", height: "40px" }} />
+        <h2
+          className="text-gray-800 text-xl font-bold text-center flex items-center"
+          htmlFor="email"
+        >
+          Portal East High
+        </h2>
+      </div>
+      <div className="text-center space-y-2 sm:text-left">
+        <label
+          className="block text-gray-300 text-sm font-bold mb-2 text-center"
+          htmlFor="email"
+        >
+          Solicitar contraseña
+        </label>
+        <div className="mb-4">
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="accountNumber"
+            type="password"
+            placeholder="Numero de cuenta"
+            value={accountNumber}
+            onChange={handleChange}
+            name="accountNumber"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button
+          onClick={handleSubmit}
+          className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-md border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2  mt-5 w-full"
+        >
+          Enviar
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default ConfirmAccountForm;
