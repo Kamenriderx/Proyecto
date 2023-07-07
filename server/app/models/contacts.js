d0377b3db4f192031699d18cd375a01aad204d15
Carlos <carlosalcerrolainez2017@gmail.com>
2023-07-07 17:10:28 -0600

    Ultima Actualizacion de Main

--- server/app/models/contacts.js
+++ server/app/models/contacts.js
@@ -38,8 +38,8 @@ const Contacts = connection.define(
   }
 );
 
-Contacts.belongsTo(USER, { foreignKey: 'user_id', as: 'user' });
-Contacts.belongsTo(USER, { foreignKey: 'contact_id', as: 'contact' });
+Contacts.belongsTo(USER, { foreignKey: 'USER_ID', as: 'user' });
+Contacts.belongsTo(USER, { foreignKey: 'CONTACT_ID', as: 'contact' });
 
 
 Contacts.sync({ force: false })
