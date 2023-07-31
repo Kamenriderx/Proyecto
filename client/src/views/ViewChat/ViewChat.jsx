53209bef163a6f35447d241a90160352dab5ec77
123javi <nguevara037@gmail.com>
2023-07-30 20:48:44 -0600

    Correcciones del chat

--- client/src/views/ViewChat/ViewChat.jsx
+++ client/src/views/ViewChat/ViewChat.jsx
@@ -1,13 +1,13 @@
 import fondoPerfil from "../../assets/fondoPerfil.jpg";
 
 import { MdSend } from "react-icons/Md";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
+
 import Buscador from "./components/Buscador";
 import { useContext, useEffect, useState } from "react";
 import { StoreContext } from "../../store/ContextExample";
 import io from "socket.io-client";
 import StudentContext from "../ViewStudent/context/StudentContext";
-import MessagesContext from "./context/Messages/MessagesContext";
-import ConversationContext from "./context/Conversation/ConversationContext";
 import OnlineList from "../../components/OnlineList";
 import { httpRequests } from "../../utils/helpers/httpRequests";
 
@@ -16,58 +16,78 @@ const ViewChat = () => {
   const [check, setCheck] = useState(false);
   const { state, dispatch } = useContext(StoreContext);
 
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      let s = io(`http://localhost:3000?token=${localStorage.getItem("token")}`);
-      setSocket(s);
-      dispatch({ type: "SOCKET", socket:s });
-    }
-  }, [localStorage.getItem("token")]);
-
-  useEffect(() => {
-    socket?.emit("sendMessage", {})
-  
-    
-  }, [])
-  
-  //contexto de usuario
-
   //contexto de estudiante
   const { stateStudent, getStudent } = useContext(StudentContext);
-  const { stateMessages, getMessages } = useContext(MessagesContext);
-
   const [showMessages, setShowMessages] = useState(false);
 
   //recibir datos del componente hijo
   const [idConversation, setIdConversation] = useState("");
   const [getMessageInput, setGetMessageInput] = useState("");
   const [chatConversation, setChatConversation] = useState(null);
-  //contexto de mensajes
+
+  const [contMessage, setContMessage] = useState(1);
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      let s = io(
+        `http://localhost:3000?token=${localStorage.getItem("token")}`
+      );
+      setSocket(s);
+      dispatch({ type: "SOCKET", socket: s });
+    }
+  }, [localStorage.getItem("token")]);
+
+  const getMessages = async (id, numPag) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${state.token}`,
+        },
+      };
+
+      const res = await httpRequests()["get"](
+        `http://localhost:3000/registro/message/${id}/${numPag}`,
+        { ...config }
+      );
+      const results = await Promise.resolve(res);
+      console.log("results: ", results.data);
+
+      setMessages(results.data.results.results);
+
+      if (!res.status && res?.response?.status !== 200) {
+        throw new Error(res.response.data.messagge);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const recibirDatoDelHijo = (id, chat) => {
+    setIdConversation(id);
+    setChatConversation(chat);
+    setContMessage(1)
+    
+    const getgetMessagesAsync = async () => {
+      await getMessages(id, 1);
+    };
+
+    getgetMessagesAsync();
+    setShowMessages(true);
+  };
 
   useEffect(() => {
     getStudent(state);
   }, [state]);
 
-  const recibirDatoDelHijo = (id, chat) => {
-    setIdConversation(id);
-    setChatConversation(chat);
-    getMessages(state, id);
-    setShowMessages(true);
-  };
-
   const messageInput = (e) => {
     setGetMessageInput(e.target.value);
   };
 
-  useEffect(() => {
-    socket?.on("getMessage",()=>{
-      stateMessages(getMessages(state, idConversation));
-    });
-  }, [socket])
-  
-
-  const sendMessage = () => {
-    httpRequests()["post"]("http://localhost:3000/registro/message/add", {
+  const sendMessage = async () => {
+    await httpRequests()["post"]("http://localhost:3000/registro/message/add", {
       body: {
         SENDER_ID: state.user.ID_USER,
         ID_CONVERSATION: idConversation,
@@ -76,8 +96,22 @@ const ViewChat = () => {
       },
     });
     setGetMessageInput("");
-    stateMessages(getMessages(state, idConversation));
-    socket.emit("sendMessage", {ID_RECEIVER: chatConversation.USER_.ID_USER})
+    socket?.emit("sendMessage", {
+      RECEIVER_ID: chatConversation.USER_.ID_USER,
+    });
+
+    await getMessages(idConversation, 1);
+  };
+
+  useEffect(() => {
+    socket?.on("getMessage", async (msg) => {
+      await getMessages(idConversation, 1);
+    });
+  }, [socket, idConversation]);
+
+  const pagination = async () => {
+    setContMessage(contMessage + 1);
+    await getMessages(idConversation, contMessage);
   };
 
   return (
@@ -91,13 +125,13 @@ const ViewChat = () => {
             <div className="w-1/2">
               <div>
                 <div className="bg-gray-200 flex gap-4 border-b-2 border-gray-500 h-16 p-2">
-                  {showMessages && stateMessages && (
+                  {showMessages && messages && (
                     <>
                       <img
                         src={
-                          chatConversation.USER_.hasOwnProperty("multimedia")
-                            ? chatConversation.USER_.multimedia[
-                                chatConversation.USER_.multimedia.length - 1
+                          chatConversation?.USER_.hasOwnProperty("multimedia")
+                            ? chatConversation?.USER_?.multimedia[
+                                chatConversation?.USER_?.multimedia?.length - 1
                               ].URL
                             : fondoPerfil
                         }
@@ -115,21 +149,18 @@ const ViewChat = () => {
                     </>
                   )}
                 </div>
-                {showMessages && stateMessages && (
+                {showMessages && messages && (
                   <>
-                    <div className="overflow-y-scroll h-[450px] flex flex-col-reverse ">
-                      {stateMessages.results.results.map((mess) => (
-                        <div key={mess.SENDER_ID+""+ state.user.ID_USER}>
+                    <div className="overflow-y-scroll h-[450px] flex flex-col-reverse">
+                      {messages.map((mess) => (
+                        <div key={mess.ID_MESSAGE}>
                           {mess.SENDER_ID == state.user.ID_USER ? (
-                            <div
-                              className="m-4 flex gap-2"
-                              key={mess.ID_MESSAGE}
-                            >
+                            <div className="m-4 flex gap-2">
                               <img
                                 src={
-                                  mess.user.hasOwnProperty("multimedia")
-                                    ? mess.user.multimedia[
-                                        mess.user.multimedia.length - 1
+                                  mess.user?.hasOwnProperty("multimedia")
+                                    ? mess.user?.multimedia[
+                                        mess.user?.multimedia?.length - 1
                                       ].URL
                                     : fondoPerfil
                                 }
@@ -139,26 +170,27 @@ const ViewChat = () => {
                               <div className="h-auto w-4/5 bg-gray-100 border border-black rounded-tr-2xl rounded-bl-2xl p-2">
                                 <p className="break-words ">{mess.CONTENT}</p>
                                 <p className="text-xs float-right">
-                                  {new Date(mess.createdAt).getHours()}:{new Date(mess.createdAt).getMinutes()}:{new Date(mess.createdAt).getSeconds()}
+                                  {new Date(mess.createdAt).getHours()}:
+                                  {new Date(mess.createdAt).getMinutes()}:
+                                  {new Date(mess.createdAt).getSeconds()}
                                 </p>
                               </div>
                             </div>
                           ) : (
-                            <div
-                              className="m-4 flex gap-2 justify-end"
-                              key={mess.ID_MESSAGE}
-                            >
+                            <div className="m-4 flex gap-2 justify-end">
                               <div className="h-auto w-4/5 bg-gray-100 border border-black rounded-tr-2xl rounded-bl-2xl p-2">
                                 <p className="break-words">{mess.CONTENT}</p>
                                 <p className="text-xs float-right">
-                                {new Date(mess.createdAt).getHours()}:{new Date(mess.createdAt).getMinutes()}:{new Date(mess.createdAt).getSeconds()}
+                                  {new Date(mess.createdAt).getHours()}:
+                                  {new Date(mess.createdAt).getMinutes()}:
+                                  {new Date(mess.createdAt).getSeconds()}
                                 </p>
                               </div>
                               <img
                                 src={
-                                  mess.user.hasOwnProperty("multimedia")
-                                    ? mess.user.multimedia[
-                                        mess.user.multimedia.length - 1
+                                  mess.user?.hasOwnProperty("multimedia")
+                                    ? mess.user?.multimedia[
+                                        mess.user?.multimedia?.length - 1
                                       ].URL
                                     : fondoPerfil
                                 }
@@ -169,6 +201,14 @@ const ViewChat = () => {
                           )}
                         </div>
                       ))}
+                      {
+                        messages.length === 15 ?
+                        (<div className="flex justify-center items-center p-1" 
+                        >
+                          <BsFillArrowUpCircleFill className=" text-blue-600 text-2xl  hover:text-blue-400 cursor-pointer"
+                          onClick={pagination}/>
+                        </div>) : '' 
+                      }
                     </div>
 
                     <div className="flex gap-4 items-center bg-gray-200 rounded-2xl">
