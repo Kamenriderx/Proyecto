336f95d859d6c7184235eafd918e7bcbd64e9dc3
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2023-08-18 16:14:32 -0600

    Cancelacion excepcional backend

--- server/app/routes/request.js
+++ server/app/routes/request.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-const {getCareers, requestChangeCareer, getRequestChangeCareer, responseRequest, cancelledRequest, getMyRequests, requestChangeCenter, getMyRequestsAcceptDenyCenter, getMyRequestsAcceptDenyCareer, getRequestChangeCenter, requestPaymentReplacement, getMyRequestsPaymentReplacements, getMyRequestsChangeCareer, getMyRequestsChangeCenter} = require('../controllers/request.js');
+const {getCareers, requestChangeCareer, getRequestChangeCareer, responseRequest, cancelledRequest, getMyRequests, requestChangeCenter, getMyRequestsAcceptDenyCenter, getMyRequestsAcceptDenyCareer, getRequestChangeCenter, requestPaymentReplacement, getMyRequestsPaymentReplacements, getMyRequestsChangeCareer, getMyRequestsChangeCenter, requestExceptionalCancellation, getMyRequestsAcceptDenyCancellCourse} = require('../controllers/request.js');
 const {checkRolCoordinador} = require('../middlewares/rol.js');
 const authMiddleware = require('../middlewares/authentication.js');
 const upload = require('../middlewares/uploadFiles.js');
@@ -17,12 +17,21 @@ router.get("/listCareers" ,getCareers);
 router.get("/getMyRequestsChangeCareer" ,authMiddleware,getMyRequestsChangeCareer);
 router.get("/getMyRequestChangeCenter" ,authMiddleware,getMyRequestsChangeCenter);
 router.get("/getMyRequestPaymentReplacements" ,authMiddleware,getMyRequestsPaymentReplacements);
+
+//  dictamenes
 router.get("/getMyDictamenCenter" ,authMiddleware,getMyRequestsAcceptDenyCenter);
 router.get("/getMyDictamenCareer" ,authMiddleware,getMyRequestsAcceptDenyCareer);
+router.get("/getMyDictamenExceptionalCancellation/:idUser" ,authMiddleware,getMyRequestsAcceptDenyCancellCourse);
+
+// solcitudes que hace el estudiante 
 router.post("/requestChangeCareer",authMiddleware, upload.single("pdfFile") ,requestChangeCareer);
 router.post("/requestChangeCenter",authMiddleware, upload.single("pdfFile"), requestChangeCenter);
+router.post("/requestExceptionalCancellation/:idUser",authMiddleware, upload.single("pdfFile"), requestExceptionalCancellation);
 router.post("/requestPaymentReplacements",authMiddleware, requestPaymentReplacement);
+
 router.put("/cancelledRequest", authMiddleware,cancelledRequest)
+
+
 
 
 module.exports = router
