ceece5dd5f4854b9f79bb95be5a093e54f7a695f
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2023-08-17 21:09:15 -0600

    actualizacion

--- server/app/routes/enrollment.js
+++ server/app/routes/enrollment.js
@@ -7,7 +7,7 @@ const {getEnrollmentAreas, enrolmentCourse, listCoursesArea, getSectionsByIdCour
 
 
 // enpoint para generar matricula
-router.post("/inscription/:idUser",verifyPaymentEnrollment , verifyEnrollment);
+router.post("/inscription/:idUser",verifyPaymentEnrollment , verifyIndexAcademic, verifyEnrollment);
 // enpoint para matricular una clase
 router.post("/inscriptionCourse/:idUser",verifyPaymentEnrollment , enrolmentCourse);
 
