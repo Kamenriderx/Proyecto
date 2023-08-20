4cf58e096f32510d87f6a6744b00b420aa5219b7
fsuazo <fsuazo124@gmail.com>
2023-08-19 22:32:18 -0600

    Subida de Notas y Evaluacion Docente

---
+++ server/app/controllers/evaluateProffesor.js
@@ -0,0 +1,102 @@
+const connection = require('../../config/database');
+const Evaluation = require('../models/evaluateProfessor');
+
+// Controlador que obtiene las secciones matriculadas de un estudiante en específico
+exports.classStudent = async function (req, res) {
+    const idStudent = req.params.idStudent;
+  
+    try {
+      const query = `
+        SELECT
+          a.ID_SECTION,
+          c.CODE_COURSE,
+          c.NAME AS COURSE_NAME,
+          a.SECTION_CODE,
+          u.NAME AS NAME_PROFFESOR,
+          e.CALIFICATION,
+          e.OBS AS OBSERVATION
+        FROM
+          ENROLLMENT e
+          INNER JOIN SECTION a ON a.ID_SECTION = e.ID_SECTION
+          INNER JOIN COURSE c ON c.ID_COURSE = a.ID_COURSE
+          INNER JOIN PROFESSOR p ON p.ID_PROFFERSSOR = a.ID_PROFFERSSOR
+          INNER JOIN USER_ u ON u.ID_USER = p.ID_USER
+        WHERE
+          e.ID_STUDENT = :idStudent
+          AND e.STATE = 'Matriculada';
+      `;
+  
+      const results = await connection.query(query, {
+        replacements: { idStudent },
+        type: connection.QueryTypes.SELECT,
+      });
+  
+      if (results.length > 0) {
+        res.json(results);
+      } else {
+        res.status(404).json({ error: 'No se encontraron secciones matriculadas para este estudiante' });
+      }
+    } catch (error) {
+      console.error('Error al obtener las secciones matriculadas:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  }
+
+  //! Controlador para evaluar docente
+  exports.updateEvaluation = async function (req, res) {
+    const { idSection } = req.params;
+  const {
+    RESP_1, RESP_2, RESP_3, RESP_4, RESP_5, RESP_6, RESP_7, RESP_8,
+    RESP_9, RESP_10, RESP_11, RESP_12, RESP_13, RESP_14, RESP_15, RESP_16,
+    RESP_17, RESP_18, RESP_19, RESP_20, RESP_21, RESP_22, RESP_23, RESP_24,
+    RESP_25, RESP_26, RESP_27, RESP_28
+  } = req.body;
+
+  try {
+    // Consulta la tabla Section para obtener los valores necesarios
+    const sectionData = await connection.query(
+      "SELECT ID_PERIOD, ID_PROFFERSSOR, ID_COURSE FROM Section WHERE ID_SECTION = ?",
+      {
+        replacements: [idSection],
+        type: connection.QueryTypes.SELECT
+      }
+    );
+
+    if (sectionData.length === 0) {
+      return res.status(404).json({ error: "La sección no existe" });
+    }
+
+    // Verifica si ya se a evaluado 
+    const existingEvaluation = await Evaluation.findOne({
+        where: {
+          ID_SECTION: idSection,
+          EVALUATED: true 
+        }
+      });
+  
+      if (existingEvaluation) {
+        return res.status(400).json({ error: "Evaluación completada con exito" });
+      }
+
+    // Crea una nueva evaluación
+    const createdEvaluation = await Evaluation.create({
+      ID_PERIOD: sectionData[0].ID_PERIOD,
+      ID_PROFFERSSOR: sectionData[0].ID_PROFFERSSOR,
+      ID_COURSE: sectionData[0].ID_COURSE,
+      ID_SECTION: idSection,
+      RESP_1, RESP_2, RESP_3, RESP_4, RESP_5, RESP_6, RESP_7, RESP_8,
+      RESP_9, RESP_10, RESP_11, RESP_12, RESP_13, RESP_14, RESP_15, RESP_16,
+      RESP_17, RESP_18, RESP_19, RESP_20, RESP_21, RESP_22, RESP_23, RESP_24,
+      RESP_25, RESP_26, RESP_27, RESP_28,
+      EVALUATED: true 
+    });
+
+    return res.status(201).json(createdEvaluation);
+  } catch (error) {
+    console.error("Error al crear la evaluación:", error);
+    return res.status(500).json({ error: "Error al crear la evaluación" });
+  }
+};
+
+
+
---
+++ server/app/routes/evaluateProffesor.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const {classStudent, updateEvaluation} = require("../controllers/evaluateProffesor");
+
+router.get("/:idStudent", classStudent)
+router.post("/evaluate/:idSection", updateEvaluation)
+module.exports = router;
